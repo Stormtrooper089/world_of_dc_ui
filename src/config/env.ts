a178d509bfb3cd1b83c6f7cb3463e764e791b412
Akshay Shirude <itsakshay95@gmail.com>
2025-12-02 11:17:43 +0530

    Update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,3 +4,4 @@ export const config = {
   appName: import.meta.env.VITE_APP_NAME || 'Complaint Management System',
   appVersion: import.meta.env.VITE_APP_VERSION || '1.0.0',
 };
+// trigger new build
